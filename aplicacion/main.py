c67e3fa5e60144d657c3c78839186c0038a35916
Kenia <23308060610371@cetis61.edu.mx>
2026-03-04 11:29:23 -0600

    x

--- aplicacion/main.py
+++ aplicacion/main.py
@@ -30,32 +30,75 @@ def main(page: ft.Page):
 
     inscripcion = ft.Checkbox(label="¿Requiere inscripción previa?")
 
-    cantidad_horas = ft.Slider(min=1,
-                        max=10,
-                        divisions=9, 
-                        value=1,
-                        label="{value}")
+    cantidad_horas = ft.Slider(
+        min=1,
+        max=10,
+        divisions=9, 
+        value=1,
+        label="{value}"
+    )
+
+
+    fecha_texto = ft.Text("No se ha seleccionado fecha")
+
+    def seleccionar_fecha(e):
+        fecha = date_picker.value
+        if fecha:
+            fecha_texto.value = f"Fecha seleccionada: {fecha.strftime('%d/%m/%Y')}"
+            page.update()
+
+    date_picker = ft.DatePicker(on_change=seleccionar_fecha)
+    page.overlay.append(date_picker)
+
+    boton_fecha = ft.ElevatedButton(
+        "Seleccionar fecha",
+        on_click=lambda e: page.open(date_picker)
+    )
+
+    lista_eventos = ft.ListView(
+        expand=True,
+        spacing=10,
+        height=200
+    )
 
     resumen = ft.Text(
         size=15,
-    color=ft.Colors.PINK,
-
+        color=ft.Colors.PINK,
     )
 
     def mostrar_resumen(e):
-        resumen.value = f"""
+
+
+        if not nombre.value:
+            resumen.value = "Error: El nombre no puede estar vacío."
+            resumen.color = ft.Colors.RED
+            page.update()
+            return
+
+        texto = f"""
 Nombre: {nombre.value}
 Evento: {tipo_evento.value}
 Modalidad: {modalidad.value}
 Inscripcion: {"Sí" if inscripcion.value else "No"}
 Cantidad De Horas: {int(cantidad_horas.value)}
+{fecha_texto.value}
 """
+
+        resumen.value = texto
+        resumen.color = ft.Colors.PINK
+
+        lista_eventos.controls.append(
+            ft.Text(texto)
+        )
+
         page.update()
 
-    boton = ft.ElevatedButton("Registrar",
-                            on_click=mostrar_resumen,
-                            bgcolor=ft.Colors.BLUE,
-                            color=ft.Colors.WHITE)
+    boton = ft.ElevatedButton(
+        "Registrar",
+        on_click=mostrar_resumen,
+        bgcolor=ft.Colors.BLUE,
+        color=ft.Colors.WHITE
+    )
 
     page.add(
         titulo,
@@ -64,9 +107,13 @@ Cantidad De Horas: {int(cantidad_horas.value)}
         modalidad,
         inscripcion,
         cantidad_horas,
+        boton_fecha,
+        fecha_texto,
         boton,
         ft.Divider(),
-        resumen
+        resumen,
+        ft.Text("Eventos guardados:"),
+        lista_eventos
     )
 
 ft.app(target=main)
